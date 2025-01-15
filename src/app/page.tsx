c194ff65b71ed016e3082b59c9ac8ad06966424e
bartoszpro <bartoszprotasewicz@gmail.com>
2025-01-15 11:01:53 -0500

    metadata changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,19 @@
 
 import React from "react";
 import Map from "./components/Map";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-white">
+      <Head>
+        <title>U.S. GDP</title>
+        <meta name="description" content="Explore U.S. GDP insights at state and county levels." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Map />
+      <SpeedInsights />
     </div>
   );
 }
